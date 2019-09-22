0e6e9e4d1a86e8c57c2485bf185eca3e819f55e8
fonsecadh <UO258318@uniovi.es>
2019-09-22 19:53:45 +0200

    Added components. Still need to work on the functionality.

--- cpm-uo258318-lab2/src/gui/RegistryWindow.java
+++ cpm-uo258318-lab2/src/gui/RegistryWindow.java
@@ -14,6 +14,10 @@ import java.awt.event.ActionEvent;
 import javax.swing.border.TitledBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.ButtonGroup;
 
 public class RegistryWindow extends JFrame {
 
@@ -28,6 +32,15 @@ public class RegistryWindow extends JFrame {
 	private JPanel orderPanel;
 	private JLabel lblNameSurname;
 	private JTextField txtNameSurname;
+	private JLabel lblBirthrate;
+	private JComboBox cbBirthrate;
+	private JLabel lblPasswd;
+	private JPasswordField pfPasswd;
+	private JLabel lblPasswdRep;
+	private JPasswordField pfPasswdRep;
+	private JRadioButton rdbtnOnSite;
+	private JRadioButton rdbtnTakeAway;
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 	
 	
 
@@ -53,7 +66,7 @@ public class RegistryWindow extends JFrame {
 	public RegistryWindow() {
 		setTitle("Customer Information");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 706, 372);
+		setBounds(100, 100, 664, 372);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -76,7 +89,7 @@ public class RegistryWindow extends JFrame {
 			btnCancel.setFont(new Font("Tahoma", Font.PLAIN, 14));
 			btnCancel.setForeground(Color.WHITE);
 			btnCancel.setBackground(new Color(220, 20, 60));
-			btnCancel.setBounds(570, 286, 89, 23);
+			btnCancel.setBounds(544, 286, 89, 23);
 		}
 		return btnCancel;
 	}
@@ -86,7 +99,7 @@ public class RegistryWindow extends JFrame {
 			btnNext.setForeground(Color.WHITE);
 			btnNext.setFont(new Font("Tahoma", Font.PLAIN, 14));
 			btnNext.setBackground(new Color(46, 139, 87));
-			btnNext.setBounds(471, 286, 89, 23);
+			btnNext.setBounds(443, 286, 89, 23);
 		}
 		return btnNext;
 	}
@@ -99,14 +112,24 @@ public class RegistryWindow extends JFrame {
 			customerInfoPanel.setLayout(null);
 			customerInfoPanel.add(getLblNameSurname());
 			customerInfoPanel.add(getTxtNameSurname());
+			customerInfoPanel.add(getLblBirthrate());
+			customerInfoPanel.add(getCbBirthrate());
+			customerInfoPanel.add(getLblPasswd());
+			customerInfoPanel.add(getPfPasswd());
+			customerInfoPanel.add(getLblPasswdRep());
+			customerInfoPanel.add(getPfPasswdRep());
 		}
 		return customerInfoPanel;
 	}
 	private JPanel getOrderPanel() {
 		if (orderPanel == null) {
 			orderPanel = new JPanel();
+			orderPanel.setBorder(new TitledBorder(null, "Order", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 			orderPanel.setBackground(Color.WHITE);
 			orderPanel.setBounds(24, 228, 375, 70);
+			orderPanel.setLayout(null);
+			orderPanel.add(getRdbtnOnSite());
+			orderPanel.add(getRdbtnTakeAway());
 		}
 		return orderPanel;
 	}
@@ -115,7 +138,7 @@ public class RegistryWindow extends JFrame {
 			lblNameSurname = new JLabel("Name and Surname:");
 			lblNameSurname.setLabelFor(getTxtNameSurname());
 			lblNameSurname.setFont(new Font("Tahoma", Font.PLAIN, 14));
-			lblNameSurname.setBounds(10, 33, 132, 17);
+			lblNameSurname.setBounds(10, 33, 157, 17);
 		}
 		return lblNameSurname;
 	}
@@ -127,5 +150,76 @@ public class RegistryWindow extends JFrame {
 			txtNameSurname.setColumns(10);
 		}
 		return txtNameSurname;
+	}
+	private JLabel getLblBirthrate() {
+		if (lblBirthrate == null) {
+			lblBirthrate = new JLabel("Birthrate:");
+			lblBirthrate.setLabelFor(getCbBirthrate());
+			lblBirthrate.setFont(new Font("Dialog", Font.PLAIN, 14));
+			lblBirthrate.setBounds(10, 67, 122, 21);
+		}
+		return lblBirthrate;
+	}
+	private JComboBox getCbBirthrate() {
+		if (cbBirthrate == null) {
+			cbBirthrate = new JComboBox();
+			cbBirthrate.setFont(new Font("Dialog", Font.BOLD, 14));
+			cbBirthrate.setBounds(185, 65, 418, 24);
+		}
+		return cbBirthrate;
+	}
+	private JLabel getLblPasswd() {
+		if (lblPasswd == null) {
+			lblPasswd = new JLabel("Password:");
+			lblPasswd.setLabelFor(getPfPasswd());
+			lblPasswd.setFont(new Font("Dialog", Font.PLAIN, 14));
+			lblPasswd.setBounds(10, 106, 122, 21);
+		}
+		return lblPasswd;
+	}
+	private JPasswordField getPfPasswd() {
+		if (pfPasswd == null) {
+			pfPasswd = new JPasswordField();
+			pfPasswd.setFont(new Font("Dialog", Font.PLAIN, 14));
+			pfPasswd.setBounds(185, 104, 418, 26);
+		}
+		return pfPasswd;
+	}
+	private JLabel getLblPasswdRep() {
+		if (lblPasswdRep == null) {
+			lblPasswdRep = new JLabel("Repeat password:");
+			lblPasswdRep.setLabelFor(getPfPasswdRep());
+			lblPasswdRep.setFont(new Font("Dialog", Font.PLAIN, 14));
+			lblPasswdRep.setBounds(10, 145, 145, 21);
+		}
+		return lblPasswdRep;
+	}
+	private JPasswordField getPfPasswdRep() {
+		if (pfPasswdRep == null) {
+			pfPasswdRep = new JPasswordField();
+			pfPasswdRep.setFont(new Font("Dialog", Font.PLAIN, 14));
+			pfPasswdRep.setBounds(185, 142, 418, 26);
+		}
+		return pfPasswdRep;
+	}
+	private JRadioButton getRdbtnOnSite() {
+		if (rdbtnOnSite == null) {
+			rdbtnOnSite = new JRadioButton("On site");
+			buttonGroup.add(rdbtnOnSite);
+			rdbtnOnSite.setBackground(Color.WHITE);
+			rdbtnOnSite.setFont(new Font("Dialog", Font.PLAIN, 14));
+			rdbtnOnSite.setBounds(53, 39, 114, 23);
+		}
+		return rdbtnOnSite;
+	}
+	private JRadioButton getRdbtnTakeAway() {
+		if (rdbtnTakeAway == null) {
+			rdbtnTakeAway = new JRadioButton("Take away");
+			buttonGroup.add(rdbtnTakeAway);
+			rdbtnTakeAway.setFont(new Font("Dialog", Font.PLAIN, 14));
+			rdbtnTakeAway.setBackground(Color.WHITE);
+			rdbtnTakeAway.setBounds(171, 39, 149, 23);
+		}
+		return rdbtnTakeAway;
 	}
 }
